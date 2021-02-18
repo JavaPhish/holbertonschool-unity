f1c7da5bc763ac25c4daa7e71736e287c8607165
Carter C <cclements3150@gmail.com>
2021-02-17 21:39:29 -0500

    Duplicated 0x05

---
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float sensitivity = 3.0f;
+
+	public Transform player;
+	
+	float x;
+	
+	void Start () {
+		transform.SetParent(player);
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		x += Input.GetAxis("Mouse X") * sensitivity;
+		transform.LookAt(player);
+		player.rotation = Quaternion.Euler(0, x, 0);
+	}
+}
