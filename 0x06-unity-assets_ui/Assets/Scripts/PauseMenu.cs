c2a3d2ca958b092103b1f79e00e91dc94a1afbf8
Carter C <cclements3150@gmail.com>
2021-03-01 19:38:12 -0500

    Added Y invert

--- 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -40,5 +41,15 @@ public class PauseMenu : MonoBehaviour {
 
 		menu.SetActive(false);
 		Time.timeScale = 1;
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Options()
+	{
+		SceneManager.LoadScene("Options");
 	}
 }
