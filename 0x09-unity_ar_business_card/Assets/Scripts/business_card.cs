cacd48e4b650314d69862b54cc6ee1dbc0b7bf47
Carter C <cclements3150@gmail.com>
2021-04-07 19:01:34 -0400

    Created layout

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/business_card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class business_card : MonoBehaviour
+{
+    public void linkedin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/carter-clements-011b47157/");
+    }
+
+    public void twitter()
+    {
+        Application.OpenURL("https://twitter.com/JavaPhish");
+    }
+
+    public void github()
+    {
+        Application.OpenURL("https://github.com/JavaPhish");
+    }
+
+    public void medium()
+    {
+        Application.OpenURL("https://cclements3150.medium.com/");
+    }
+}
