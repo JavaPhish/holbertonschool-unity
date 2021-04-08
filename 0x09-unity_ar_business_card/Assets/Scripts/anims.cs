89567fbd532c0ad65de049b042633415de6fb360
Carter C <cclements3150@gmail.com>
2021-04-07 21:42:25 -0400

    Finished

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/anims.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class anims : MonoBehaviour, ITrackableEventHandler
+{
+    public Animator twitter_anim;
+    public Animator medium_anim;
+    public Animator github_anim;
+    public Animator linkedin_anim;
+    private TrackableBehaviour trackableBehaviour;
+
+    void Start()
+    {
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (trackableBehaviour)
+            trackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+      TrackableBehaviour.Status previousStatus,
+      TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            OnTrackingFound();
+
+    }
+
+    void OnTrackingFound()
+    {
+        twitter_anim.Play("Twitter", 0, 0);
+        medium_anim.Play("medium", 0, 0);
+        linkedin_anim.Play("linkeding", 0, 0);
+        github_anim.Play("github_slide", 0, 0);        
+    }
+}
