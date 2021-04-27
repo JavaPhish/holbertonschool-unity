48744a530f46c2e25acb39155ac9c2a7df481a76
Carter C <cclements3150@gmail.com>
2021-04-27 16:57:27 -0400

    Made the SFX and BGM sliders work

---
+++ 0x08-unity-audio/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour {
+
+	public AudioMixer mix;
+
+	public void SetBGMLevel(float value)
+	{
+		mix.SetFloat("BGM", Mathf.Log10(value) * 20.0f);
+	}
+
+	public void SetSFXLevel(float value)
+	{
+		mix.SetFloat("SFX", Mathf.Log10(value) * 20.0f);
+	}
+}
