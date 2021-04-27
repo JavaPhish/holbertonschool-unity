f17954cc7e0588e385a2d6f56ed6bb43488e9c26
Carter C <cclements3150@gmail.com>
2021-04-27 16:24:04 -0400

    Added a "splat" sound when the user respawns and falls to the ground

--- 0x08-unity-audio/Assets/Scripts/animEvent.cs
+++ 0x08-unity-audio/Assets/Scripts/animEvent.cs
@@ -6,8 +6,15 @@ public class animEvent : MonoBehaviour {
 
 	public FootstepController thing;
 
+	public AudioSource fallGround;
+
 	void playStepSound()
 	{
 		thing.playStepSound();
+	}
+
+	void splat()
+	{
+		fallGround.Play(0);
 	}
 }
