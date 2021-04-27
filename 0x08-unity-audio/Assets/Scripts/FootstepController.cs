cbf3aa898b62a7f1da853deec7d2ef1273bee015
Carter C <cclements3150@gmail.com>
2021-04-27 16:09:34 -0400

    Added foot step sounds

---
+++ 0x08-unity-audio/Assets/Scripts/FootstepController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepController : MonoBehaviour {
+
+	private bool onGround;
+	public CharacterController controller;
+	private string floorType = "None";
+
+	public AudioSource grass;
+	public AudioSource rock; 
+
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(floorType);
+		onGround = controller.isGrounded;
+
+	}
+
+	void playStepSound()
+	{
+		if (!onGround)
+			return;
+
+		if (floorType == "Grass")
+		{
+			grass.Play(0);
+		}
+		else
+		{
+			rock.Play(0);
+		}
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+		// Debug.Log(hit.gameObject.tag);
+		floorType = hit.gameObject.tag;
+    }
+}
