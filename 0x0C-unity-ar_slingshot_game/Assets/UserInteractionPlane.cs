eca01ee64647a446c5ec36879539c498649df324
Carter C <cclements3150@gmail.com>
2021-06-01 18:12:18 -0400

    Looks for a plane and lets the user select

--- 0x0C-unity-ar_slingshot_game/Assets/UserInteractionPlane.cs
+++ 0x0C-unity-ar_slingshot_game/Assets/UserInteractionPlane.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
 
 public class UserInteractionPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private ARPlaneManager planes;
+
+    public GameObject searching;
+    public GameObject select;
+
     void Start()
     {
-        
+        planes = GetComponent<ARPlaneManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (planes.trackables.count > 0)
+        {
+            searching.SetActive(false);
+            select.SetActive(true);
+        }
     }
 }
